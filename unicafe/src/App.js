61df64462b1a6b50c1bf536df0ffd0a1b3edadcf
11amanraj <110328977+11amanraj@users.noreply.github.com>
2022-11-10 22:30:11 +0530

    Exercise 1.9
    Unicafe step4

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -3,28 +3,30 @@ import { useState } from 'react'
 const Statistics = ({good,neutral,bad}) => {
   const totalFeedback = good+neutral+bad;
 
-  let average;
-  let percentage;
+  let average = (good*1 + neutral*0 - bad*1)/totalFeedback;
+  let percentage = (good* 100)/totalFeedback;
 
-  if(totalFeedback === 0) {
-    average = 0;
-    percentage = 0;
+  if(!totalFeedback) {
+    return (
+      <>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </>
+    )
   } else {
-    average = (good*1 + neutral*0 - bad*1)/totalFeedback;
-    percentage = (good* 100)/totalFeedback;
+    return (
+      <>
+        <h2>statistics</h2>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {good + neutral + bad}</p>
+        <p>average {average}</p>
+        <p>positive {percentage} %</p>
+      </>
+    )
   }
 
-  return (
-    <>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {average}</p>
-      <p>positive {percentage} %</p>
-    </>
-  )
 }
 
 const App = () => {
@@ -33,17 +35,18 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return (
-    <div>
-      <h2>give feedback</h2>
+    return (
       <div>
-        <button onClick={() => setGood(good+1)}>good</button>
-        <button onClick={() => setNeutral(neutral+1)}>neutral</button>
-        <button onClick={() => setBad(bad+1)}>bad</button>
+        <h2>give feedback</h2>
+        <div>
+          <button onClick={() => setGood(good+1)}>good</button>
+          <button onClick={() => setNeutral(neutral+1)}>neutral</button>
+          <button onClick={() => setBad(bad+1)}>bad</button>
+        </div>
+        <Statistics {...{good,neutral,bad}}/>
       </div>
-      <Statistics {...{good,neutral,bad}}/>
-    </div>
-  )
+    )
+
 }
 
 export default App;
